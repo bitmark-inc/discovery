15e3b9bd205cc179b7cd805794095dcffa412a4a
Linzy Hu <linzy.hu@gmail.com>
2017-06-27 15:46:47 +0800

    add go program version info

---
+++ version/version.go
@@ -0,0 +1,12 @@
+// Copyright (c) 2014-2017 Bitmark Inc.
+// Use of this source code is governed by an ISC
+// license that can be found in the LICENSE file.
+
+package version
+
+// ensure that git has a tag: "vX.Y" corresponding to major and minor
+const (
+	Major   = "0"
+	Minor   = "1"
+	Version = Major + "." + Minor
+)
